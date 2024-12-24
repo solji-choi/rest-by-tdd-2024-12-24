bf40eb2f78ed5dce2da0913b84064e53bea6f0bc
solji <solji.choi89@gmail.com>
2024-12-24 15:45:40 +0900

    login1 r

--- src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/member/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -64,4 +64,36 @@ public class ApiV1MemberControllerTest {
         Member member = memberService.findByUsername("usernew").get();
         assertThat(member.getNickname()).isEqualTo("무명");
     }
+
+    @Test
+    @DisplayName("로그인")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/api/v1/members/login")
+                        .content("""
+                                {
+                                    "username": "user1",
+                                    "password": "1234"
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("유저1님 환영합니다."))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.item").exists())
+                .andExpect(jsonPath("$.data.item.id").isNumber())
+                .andExpect(jsonPath("$.data.item.createDate").isString())
+                .andExpect(jsonPath("$.data.item.modifyDate").isString())
+                .andExpect(jsonPath("$.data.item.nickname").value("유저1"))
+                .andExpect(jsonPath("$.data.apiKey").isString());
+    }
 }
