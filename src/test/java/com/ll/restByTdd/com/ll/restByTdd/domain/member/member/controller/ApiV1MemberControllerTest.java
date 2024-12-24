6d512196e24853c4963c230ade7b440ad5a14ea0
solji <solji.choi89@gmail.com>
2024-12-24 14:23:24 +0900

    join test r

---
+++ src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -0,0 +1,35 @@
+package com.ll.restByTdd.com.ll.restByTdd.domain.member.member.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1MemberControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("회원가입")
+    void t1() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/api/v1/members/join"))
+                .andDo(print());
+
+        resultActions
+                .andExpect(status().isCreated());
+    }
+}
