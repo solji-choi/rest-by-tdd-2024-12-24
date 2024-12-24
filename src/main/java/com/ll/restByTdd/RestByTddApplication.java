6f6efce8c1b3c4fe9f30db06a3f1e88577e2fa72
solji <solji.choi89@gmail.com>
2024-12-24 12:31:13 +0900

    setting

---
+++ src/main/java/com/ll/restByTdd/RestByTddApplication.java
@@ -0,0 +1,13 @@
+package com.ll.restByTdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestByTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestByTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/restByTdd/RestByTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.restByTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestByTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
