f94efdec7db4d9973ab4e97bff9fdee9466e60f6
solji <solji.choi89@gmail.com>
2024-12-24 14:58:53 +0900

    join3 r

--- src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/member/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,12 +30,25 @@ public class ApiV1MemberControllerTest {
     @DisplayName("회원가입")
     void t1() throws Exception {
         ResultActions resultActions = mvc
-                .perform(post("/api/v1/members/join"))
+                .perform(post("/api/v1/members/join")
+                        .content("""
+                                {
+                                    "username": "usernew",
+                                    "password": "1234",
+                                    "nickname": "무명"
+                                }
+                                """.stripIndent())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
                 .andDo(print());
 
         resultActions
                 .andExpect(handler().handlerType(ApiV1MemberController.class))
                 .andExpect(handler().methodName("join"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("무명님 환영합니다. 회원가입이 완료되었습니다."));
     }
 }
