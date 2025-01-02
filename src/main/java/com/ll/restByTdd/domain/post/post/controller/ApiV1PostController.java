39e800dd82468752bb5b0f440077ec516a60aec3
solji <solji.choi89@gmail.com>
2025-01-02 12:13:02 +0900

    t : post item : r

--- src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
@@ -1,6 +1,10 @@
 package com.ll.restByTdd.domain.post.post.controller;
 
+import com.ll.restByTdd.domain.post.post.dto.PostDto;
+import com.ll.restByTdd.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
+    private final PostService postService;
 
+    @GetMapping("/{id}")
+    public PostDto item(@PathVariable long id) {
+        return null;
+    }
 }
