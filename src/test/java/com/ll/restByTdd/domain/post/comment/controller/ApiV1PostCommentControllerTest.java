f4fd02212c7ebd75eebdee4df83eefd5b657105b
solji <solji.choi89@gmail.com>
2025-01-05 01:15:15 +0900

    t, r, DELETE /api/v1/posts/1/comments/1

--- src/test/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentControllerTest.java
+++ src/test/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentControllerTest.java
@@ -1,5 +1,6 @@
 package com.ll.restByTdd.domain.post.comment.controller;
 
+import com.ll.restByTdd.domain.member.member.entity.Member;
 import com.ll.restByTdd.domain.member.member.service.MemberService;
 import com.ll.restByTdd.domain.post.comment.entity.PostComment;
 import com.ll.restByTdd.domain.post.post.service.PostService;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -64,5 +66,27 @@ public class ApiV1PostCommentControllerTest {
                     .andExpect(jsonPath("$[%d].authorName".formatted(i)).value(postComment.getAuthor().getName()))
                     .andExpect(jsonPath("$[%d].content".formatted(i)).value(postComment.getContent()));
         }
+    }
+
+    @Test
+    @DisplayName("다건 삭제")
+    void t2() throws Exception {
+        Member author = memberService.findByUsername("user2").get();
+
+        ResultActions resultActions = mvc
+                .perform(delete("/api/v1/posts/1/comments/1")
+                        .header("Authorization", "Bearer " + author.getApiKey())
+                        .contentType(
+                                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                        )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostCommentController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("1번 댓글이 삭제되었습니다.")));
     }
 }
