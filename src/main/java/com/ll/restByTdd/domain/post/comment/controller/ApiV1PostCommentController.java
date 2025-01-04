e721e049499663984e5a714c051fc1eb4b75345c
solji <solji.choi89@gmail.com>
2025-01-05 01:33:53 +0900

    t, g, PUT /api/v1/posts/1/comments/1

--- src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
@@ -8,7 +8,10 @@ import com.ll.restByTdd.domain.post.post.service.PostService;
 import com.ll.restByTdd.global.exceptions.ServiceException;
 import com.ll.restByTdd.global.rq.Rq;
 import com.ll.restByTdd.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -56,6 +59,39 @@ public class ApiV1PostCommentController {
         return new RsData<>(
                 "200-1",
                 "%d번 댓글이 삭제되었습니다.".formatted(id)
+        );
+    }
+
+    record PostCommentModifyReqBody(
+            @NotBlank
+            @Length(min = 2, max = 10000000)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    public RsData<PostCommentDto> modify (
+            @PathVariable long postId,
+            @PathVariable long id,
+            @RequestBody @Valid PostCommentModifyReqBody reqBody
+    ) {
+        Member author = rq.checkAuthentication();
+
+        Post post = postService.findById(postId).orElseThrow(
+                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
+        );
+
+        PostComment postComment = post.getCommentById(id).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
+        );
+
+        postComment.checkActorCanModify(author);
+        postComment.modify(reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 수정되었습니다.".formatted(id),
+                new PostCommentDto(postComment)
         );
     }
 }
